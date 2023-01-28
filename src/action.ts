767902774057755e1e07120d78901629c826d950
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2023-01-28 21:05:27 +0800

    feat: add action-commit
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

--- src/action.ts
+++ src/action.ts
@@ -1,6 +1,7 @@
 export const Actions = {
   "action-initial-registration": "bafkreiavifzn7ntlb6p2lr55k3oezo6pofwvknecukc5auhng4miowcte4",
   "action-initial-registration-jade": "bafkreicptxn6f752c4pvb6gqwro7s7wb336idkzr6wmolkifj3aafhvwii",
+  "action-commit" :"bafkreiceiqfe4w4g4j46rsi4h4tbqu35ivewfxm4q743tfmegscpg7d63m",
   "action-mint-erc721-nft-ethereum": "bafkreighzhmelvwlntigjq6ushun4mz36x6cq5nztkvcac47bdxz2ipeca",
   "action-mint-erc721-nft-avalanche": "bafkreibro4v4vsibvr47uwhkj6gqpc5rfvvq5ykfzzz4jnzvm23zwoh2gq",
   "action-mint-erc721-nft-polygon": "bafkreigdxfmwhhepusyhaoidyzy6bv7jzpc5liqohyc6numgvvbfwabn6a",
